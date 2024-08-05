66742ac89fbd96aeef46765f0e92af064801d745
juliesun <sjsunyy@gmail.com>
2024-08-05 17:29:27 -0400

    committed

---
+++ Coop.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Coop {
+
+    public static Map<String, Integer> mkCoopOrder(List<Ingredient> pantry_items,
+                                                   List<Ingredient> recipe_items,
+                                                   List<Order> orders) {
+        // TODO: complete this function
+        Map<String, Map<String,Integer>> pantryItems = new HashMap<>();
+        for (Ingredient ingredient : pantry_items) {
+            String key= ingredient.key;
+            String name = ingredient.name;
+            int amount = ingredient.amount;
+            pantryItems.putIfAbsent(key, new HashMap<>());
+            Map<String, Integer> nameAmount = pantryItems.get(key);
+            nameAmount.put(name, amount);
+            pantryItems.put(key, nameAmount);
+        }
+
+        Map<String, Map<String,Integer>> recipeItems = new HashMap<>();
+        for (Ingredient ingredient : recipe_items) {
+            String key= ingredient.key;
+            String name = ingredient.name;
+            int amount = ingredient.amount;
+            recipeItems.putIfAbsent(key, new HashMap<>());
+            Map<String, Integer> nameAmount = recipeItems.get(key);
+            nameAmount.put(name, amount);
+            pantryItems.put(key, nameAmount);
+        }
+        Map<String,Integer> orderItems = new HashMap<>();
+        for (Order order : orders) {
+            String familyName=order.family_name;
+            String recipeName=order.recipe_name;
+
+            Map<String, Integer> recipeIngredients=recipeItems.get(recipeName);
+            Map<String, Integer> familyPantry=pantryItems.getOrDefault(familyName, new HashMap<>());
+            for (Map.Entry<String, Integer> entry : recipeIngredients.entrySet()) {
+                String ingredientName = entry.getKey();
+                int quantity = entry.getValue();
+                int pantryIngredient=pantryItems.get(ingredientName).getOrDefault(ingredientName, 0);
+                int quantityNeeded=quantity-pantryIngredient;
+                if (quantityNeeded>0) {
+                    orderItems.put(ingredientName, quantity);
+                }
+            }
+        }
+        return orderItems;
+    }
+}
---
+++ Order.java
@@ -0,0 +1,18 @@
+/**
+        * Order: represents an order for the coop problem.
+ **/
+
+public class Order {
+    public String family_name;
+    public String recipe_name;
+
+    public Order(String family, String recipe) {
+        this.family_name = family;
+        this.recipe_name = recipe;
+    }
+
+
+    public String toString() {
+        return "order: " + this.family_name + " " + this.recipe_name;
+    }
+}
